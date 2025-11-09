40e2856a4e2a862c0450a2ebec766533ff6d9254
Sarthika252 <sarthikasuryawanshi@gmail.com>
2025-11-09 18:08:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,6 @@
 // const PORT = 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -30,14 +28,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/collegewebsite', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected successfully"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+// mongoose.connect('mongodb://localhost:27017/collegewebsite', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 // mongoose.connect("mongodb://127.0.0.1:27017/collegeDB")
 //   .then(() => console.log("MongoDB Connected"))
 //   .catch(err => console.error(err));
 
 app.use('/api/auth', authRoutes);
+
 
 app.listen(5000, () => console.log('Server running on port 5000'));
