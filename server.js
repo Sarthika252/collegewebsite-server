01dc1ec340a9af0866789e89ab2f1d75045053c7
Sarthika252 <sarthikasuryawanshi@gmail.com>
2025-11-09 18:54:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes"; // note the .js extension
+import authRoutes from "./routes/authRoutes.js"; // note the .js extension
 
 dotenv.config();
 
@@ -70,4 +70,5 @@ app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
