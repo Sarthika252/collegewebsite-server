2c64105d3f181e5f3fede5d24216d49b1e3daffe
Sarthika252 <sarthikasuryawanshi@gmail.com>
2025-11-09 20:00:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,10 @@ import authRoutes from "./routes/authRoutes.js"; // note the .js extension
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+ origin: "https://collegewebsite-client.vercel.app/",
+ methods: ["GET", "POST"]
+}));
 app.use(express.json());
 
 mongoose
@@ -70,5 +73,6 @@ app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
